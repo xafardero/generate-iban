ed4d47fa574e1d97c0d1e45665ececb21e130fb8
xaf <xavier.mabras@protonmail.ch>
2016-11-15 23:39:06 +0100

    Initial commit     Generate iban code and make a initial test

---
+++ src/Iban.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace IbanGenerator;
+
+/**
+ * Class Iban
+ *
+ * Description
+ *
+ * @link
+ */
+class Iban
+{
+    /**
+     * Iban validation.
+     *
+     * @param string $paisCode
+     * @param mixed $control
+     *
+     * @return bool
+     */
+    public function generate($countryCode, $ccc)
+    {
+
+        $number = $ccc 
+                   . $this->code(substr($countryCode, 0, 1))
+                   . $this->code(substr($countryCode, 1, 1))
+                   . '00';
+
+        $checksum =  98 - bcmod($number, '97');
+
+        if (strlen($checksum) === 1) {
+            $checksum = '0' . $checksum;
+        }
+
+        return $countryCode . $checksum;
+    }
+
+    private function code($letter)
+    {
+        $tabla = [
+            'A' => '10',
+            'B' => '11',
+            'C' => '12',
+            'D' => '13',
+            'E' => '14',
+            'F' => '15',
+            'G' => '16',
+            'H' => '17',
+            'I' => '18',
+            'J' => '19',
+            'K' => '20',
+            'L' => '21',
+            'M' => '22',
+            'N' => '23',
+            'O' => '24',
+            'P' => '25',
+            'Q' => '26',
+            'R' => '27',
+            'S' => '28',
+            'T' => '29',
+            'U' => '30',
+            'V' => '31',
+            'W' => '32',
+            'X' => '33',
+            'Y' => '34',
+            'Z' => '35' 
+        ];
+
+        return $tabla[$letter];
+    }
+}
